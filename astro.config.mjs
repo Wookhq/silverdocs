b80fbc0ae19a788c47799378449e5918c7078839
hello everyone i'm chip today we are going to play <itsjustchipoverhere@proton.me>
2025-09-02 08:38:35 +0700

    add to sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,6 +22,7 @@ export default defineConfig({
 					items : [
 						{ label: 'Introduction', slug: 'lution/introduction' },
 						{ label: 'Installation', slug: 'lution/installation' },
+						{ label: 'FAQ', slug: 'lution/faq' },
 					],
 				},
 				{
