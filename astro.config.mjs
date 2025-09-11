db7cf00ddb4487021a3e69755a048d4a10347965
debian chip <itsjustchipoverhere@proton.me>
2025-09-11 01:19:59 -0400

    revamp config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,39 +2,45 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Lution',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/wookhq/lution' }, { icon: 'discord', label: 'Discord', href: 'https://discord.gg/BXT7FYjTBa' }],
-			customCss: [
-				'./src/styles/global.css',
-				  '@fontsource-variable/lexend/index.css',
-			],
-			logo: {
-				src: './src/assets/lutiontext.svg',
-				replacesTitle: true,
-			},
-			sidebar: [
-				{
-					label : 'Lution',
-					items : [
-						{ label: 'Introduction', slug: 'lution/introduction' },
-						{ label: 'Installation', slug: 'lution/installation' },
-						{ label: 'Uninstalling Lution', slug: 'lution/uninstall' },
-						{ label: 'FAQ', slug: 'lution/faq' },
-						{ label: 'Reviews', slug: 'funstuff/reviews' },
-					],
-				},
-				{
-					label : 'Lution Marketplace',
-					items : [
-						{ label: 'Add your own theme/mods into the marketplace', slug: 'lutionmarketplace/addtheme' },
-						{ label: 'Create your own marketplace provider', slug: 'lutionmarketplace/ownmrk' },
-					],
-				}
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'Lution',
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/wookhq/lution' }, { icon: 'discord', label: 'Discord', href: 'https://discord.gg/BXT7FYjTBa' }],
+          customCss: [
+              './src/styles/global.css',
+                '@fontsource-variable/lexend/index.css',
+          ],
+          logo: {
+              src: './src/assets/lutiontext.svg',
+              replacesTitle: true,
+          },
+          sidebar: [
+              {
+                  label : 'Lution',
+                  items : [
+                      { label: 'Introduction', slug: 'lution/introduction' },
+                      { label: 'Installation', slug: 'lution/installation' },
+                      { label: 'Uninstalling Lution', slug: 'lution/uninstall' },
+                      { label: 'FAQ', slug: 'lution/faq' },
+                      { label: 'Reviews', slug: 'funstuff/reviews' },
+                  ],
+              },
+              {
+                  label : 'Lution Marketplace',
+                  items : [
+                      { label: 'Add your own theme/mods into the marketplace', slug: 'lutionmarketplace/addtheme' },
+                      { label: 'Create your own marketplace provider', slug: 'lutionmarketplace/ownmrk' },
+                  ],
+              }
+          ],
+      }),
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
