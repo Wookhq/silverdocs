43542b62933383a2a248ad3ba1e5bffe373568e9
crafty chip <itsjustchipoverhere@pront.me>
2025-09-27 10:51:53 +0700

    yay

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   integrations: [
       starlight({
-          title: 'Lution',
-          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/wookhq/lution' }, { icon: 'discord', label: 'Discord', href: 'https://discord.gg/BXT7FYjTBa' }],
+          title: 'Silver',
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/wookhq/Silverr' }, { icon: 'discord', label: 'Discord', href: 'https://discord.gg/BXT7FYjTBa' }],
           customCss: [
               './src/styles/global.css',
                 '@fontsource-variable/lexend/index.css',
@@ -20,20 +20,13 @@ export default defineConfig({
           },
           sidebar: [
               {
-                  label : 'Lution',
+                  label : 'Silver',
                   items : [
                       { label: 'Introduction', slug: 'lution/introduction' },
                       { label: 'Installation', slug: 'lution/installation' },
                       { label: 'Uninstalling Lution', slug: 'lution/uninstall' },
                       { label: 'FAQ', slug: 'lution/faq' },
                       { label: 'Reviews', slug: 'funstuff/reviews' },
-                  ],
-              },
-              {
-                  label : 'Lution Marketplace',
-                  items : [
-                      { label: 'Add your own theme/mods into the marketplace', slug: 'lutionmarketplace/addtheme' },
-                      { label: 'Create your own marketplace provider', slug: 'lutionmarketplace/ownmrk' },
                   ],
               }
           ],
